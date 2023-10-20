5691ac0e5504908efc3581a30c1da9b27808f54c
MerlinMDV <64904743+MerlinMDV@users.noreply.github.com>
2023-10-20 12:09:55 -0500

    update game.rkt

--- game.rkt
+++ game.rkt
@@ -6,21 +6,24 @@
 ;;cat is 195 pixels wide and 214 high
 ;;ct is cat top, cb is cat bottom
 (define cat (scale 0.8 (bitmap "./cat.png")))
+(define mouse (flip-horizontal (bitmap "./mouse.png")))
 
 (define posx 78)
 (define posy 450)
 (define ct (+ posy 86))
 (define cb (- posy 86))
 
+
 (define (move img k)
-  (cond [(key=? k "w") (set! posy (- posy 10)) img]
-        [(key=? k "s") (set! posy (+ posy 10)) img]
-        [else (printf "stop~n") img]
+  (cond [(key=? k "w") (begin (set! posy (- posy 10)) (set! ct (- posy 86)) (set! cb (+ posy 86))) img]
+        [(key=? k "s") (begin (set! posy (+ posy 10)) (set! ct (- posy 86)) (set! cb (+ posy 86))) img]
+        [else (begin (printf "CAT TOP IS ~a ~n" ct) (printf "CAT IS AT ~a ~n" posy) (printf "CAT BOTTOM IS ~a ~n" cb)) img]
   )
 
 )
 
 (place-image cat 78 posy scene)
+(place-image mouse 1300 posy scene)
 
 (define (drawframe img) (place-image img 78 posy scene))
 
